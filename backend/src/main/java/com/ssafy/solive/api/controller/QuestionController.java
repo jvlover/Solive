e2cef7f1e0d45a1861cf6935989e179c85f5fc13
노성호 <shtjdgh00@gmail.com>
2023-08-03 14:25:27 +0900

    Fix: 컨트롤러에 CrossOrigin 추가
    컨트롤러에 Annotation으로 CrossOrigin 추가

--- backend/src/main/java/com/ssafy/solive/api/controller/QuestionController.java
+++ backend/src/main/java/com/ssafy/solive/api/controller/QuestionController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @RestController
 @RequestMapping("/question")
+@CrossOrigin("*")
 public class QuestionController {
 
     private static final String SUCCESS = "success";  // API 성공 시 return
