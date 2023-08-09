f31be55753fed80f1f3f0a51648d79741b1d04e7
박민혁 <sis03090@naver.com>
2023-08-09 12:37:12 +0900

    Feat: 새로고침 시 login 상태 유지 구현
    - localstorage에 user 정보 저장

---
+++ frontend/src/recoil/localStorageEffect.tsx
@@ -0,0 +1,22 @@
+const localStorageEffect =
+  (key: string) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    } else {
+      localStorage.removeItem(key);
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('refreshToken');
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login';
+      }
+    }
+    onSet((newValue, _, isReset) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
+export default localStorageEffect;
