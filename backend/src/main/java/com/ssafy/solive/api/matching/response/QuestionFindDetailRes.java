1ede2aa0eaaa7e2229475a0e6810e38f70ee5023
노성호 <shtjdgh00@gmail.com>
2023-08-10 17:35:00 +0900

    Fix: question request 수정
    question request 요구사항에 맞춰서 수정

--- backend/src/main/java/com/ssafy/solive/api/matching/response/QuestionFindDetailRes.java
+++ backend/src/main/java/com/ssafy/solive/api/matching/response/QuestionFindDetailRes.java
@@ -20,13 +20,16 @@ public class QuestionFindDetailRes {
     String description;
 
     // 문제 이미지
-    List<String> imagePathName;
+    List<String> path;
 
     // 문제 분류(마스터코드)
-    Integer masterCodeId;
+    String masterCodeName;
 
     // 문제 등록 시간
     String createTime;
+
+    // 문제 매칭 상태
+    String state;
 
     public QuestionFindDetailRes() {
 
@@ -34,11 +37,20 @@ public class QuestionFindDetailRes {
 
     // Querydsl을 위한 생성자
     public QuestionFindDetailRes(String userNickname, String title, String description,
-        Integer masterCodeId, LocalDateTime createTime) {
+        String masterCodeName, LocalDateTime createTime, Integer state) {
         this.userNickname = userNickname;
         this.title = title;
         this.description = description;
-        this.masterCodeId = masterCodeId;
+        this.masterCodeName = masterCodeName;
         this.createTime = createTime.toString();
+
+        // 문제가 매칭 되었으면 2, 강사들의 지원이 있는 상태면 1, 문제가 등록만 된 초기 상태면 0
+        if (state.equals(0)) {
+            this.state = "등록됨";
+        } else if (state.equals(1)) {
+            this.state = "요청됨";
+        } else {
+            this.state = "완료됨";
+        }
     }
 }
