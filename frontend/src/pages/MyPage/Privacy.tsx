cebb5b0434d4dcfac136c65d63d349498eff40dd
박민혁 <sis03090@naver.com>
2023-08-10 14:06:03 +0900

    Feat: 개인정보 페이지 CSS 적용
    - solive 시그니처 색상 적용 및 카드 형식으로 변경

--- frontend/src/pages/MyPage/Privacy.tsx
+++ frontend/src/pages/MyPage/Privacy.tsx
@@ -7,6 +7,7 @@ import {
 } from '../../api';
 import { useRecoilState } from 'recoil';
 import { userState } from '../../recoil/user/userState';
+import { Card, CardBody } from '@material-tailwind/react';
 
 const PersonalInfoPage = () => {
   const [user, setUser] = useRecoilState(userState);
@@ -95,93 +96,80 @@ const PersonalInfoPage = () => {
 
   return (
     <div>
-      <div
-        className="mx-auto mt-8 h-[650px] w-[600px] p-6"
-        style={{ border: '2px solid #646CFF' }}
-      >
-        <div className="flex items-center justify-between w-full mt-4 font-bold">
-          <div className="flex items-center">이메일</div>
-        </div>
-        <div className="w-full p-2 mt-4 bg-blue-200 border border-gray-300 rounded">
-          <div className="flex items-center">{email}</div>
-        </div>
-        <h2 className="mt-4 font-bold">비밀번호 변경</h2>
-        <label className="block w-full mt-4">
-          <input
-            type="password"
-            name="oldPassword"
-            placeholder="현재 비밀번호"
-            onChange={handleInputChange}
-            className="w-full p-2 bg-blue-200 border border-gray-300 rounded "
-          />
-        </label>
-        <label className="block w-full mt-4">
-          <input
-            type="password"
-            name="newPassword"
-            placeholder="새 비밀번호"
-            onChange={handleInputChange}
-            className="w-full p-2 bg-blue-200 border border-gray-300 rounded"
-          />
-        </label>
-        <label className="block w-full mt-4">
-          <input
-            type="password"
-            name="confirmNewPassword"
-            placeholder="새 비밀번호 재입력"
-            onChange={handleInputChange}
-            className="w-full p-2 bg-blue-200 border border-gray-300 rounded "
-          />
-        </label>
-        <div className="flex items-center justify-between w-full mt-4 font-bold">
-          <div className="flex items-center">회원가입일자</div>
-        </div>
-        <div className="flex items-center justify-between w-full ">
-          <div className="w-full p-2 mt-4 bg-blue-200 border border-gray-300 rounded">
-            {signinTime}
+      <Card className="my-5 min-h-[80vh] w-[80vh] p-6">
+        <CardBody>
+          <div className="flex items-center justify-between w-full font-bold">
+            <div className="flex items-center">이메일</div>
           </div>
-        </div>
+          <div className="w-full p-2 mt-4 border border-gray-300 rounded bg-solive-200 bg-opacity-30">
+            <div className="flex items-center">{email}</div>
+          </div>
+          <h2 className="mt-8 font-bold">비밀번호 변경</h2>
+          <label className="block w-full mt-4">
+            <input
+              type="password"
+              name="oldPassword"
+              placeholder="현재 비밀번호"
+              onChange={handleInputChange}
+              className="w-full p-2 border border-gray-300 rounded bg-solive-200 bg-opacity-30 "
+            />
+          </label>
+          <label className="block w-full mt-4">
+            <input
+              type="password"
+              name="newPassword"
+              placeholder="새 비밀번호"
+              onChange={handleInputChange}
+              className="w-full p-2 border border-gray-300 rounded bg-solive-200 bg-opacity-30"
+            />
+          </label>
+          <label className="block w-full mt-4">
+            <input
+              type="password"
+              name="confirmNewPassword"
+              placeholder="새 비밀번호 확인"
+              onChange={handleInputChange}
+              className="w-full p-2 border border-gray-300 rounded bg-solive-200 bg-opacity-30 "
+            />
+          </label>
+          <div className="flex items-center justify-between w-full mt-8 font-bold">
+            <div className="flex items-center">회원가입일자</div>
+          </div>
+          <div className="flex items-center justify-between w-full ">
+            <div className="w-full p-2 mt-4 border border-gray-300 rounded bg-solive-200 bg-opacity-30">
+              {signinTime.replace('T', ' ')}
+            </div>
+          </div>
 
-        <form onSubmit={handleSubmit} className="flex justify-center">
-          <button
-            className={`mt-8 px-4 py-2 rounded w-64 h-12 ${
-              isModified ? 'bg-blue-600' : 'bg-gray-400'
-            }`}
-            type="submit"
-          >
-            저장하기
-          </button>
-        </form>
-      </div>
-      <div
-        className="mx-auto mt-8 h-[100px] w-[600px] p-6"
-        style={{ border: '2px solid #646CFF' }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            height: '100%',
-          }}
-        >
+          <form onSubmit={handleSubmit} className="flex justify-center">
+            <button
+              className={`mt-10 px-4 py-2 rounded w-64 h-12 ${
+                isModified ? 'bg-solive-200' : 'bg-gray-400'
+              }`}
+              type="submit"
+              disabled={!isModified}
+            >
+              비밀번호 변경
+            </button>
+          </form>
+        </CardBody>
+      </Card>
+      <Card className="mx-auto mb-8 h-[100px] w-[80vh] p-6">
+        <div className="flex items-center justify-between h-full">
           <p className="font-bold">회원 탈퇴</p>
           <button onClick={() => setShowPopup(true)}>➔</button>
         </div>
-      </div>
+      </Card>
       {showPopup && (
         <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-black bg-opacity-40">
-          <div
-            className="mx-auto mt-8 h-[450px] w-[800px] p-6 bg-white rounded-lg "
-            style={{ border: '2px solid #646CFF' }}
-          >
+          <div className="flex flex-col items-center justify-center mx-auto mt-8 h-[300px] w-[600px] p-6 bg-white rounded-lg border-[1px] border-solive-200">
             <h2 className="font-bold text-center" style={{ fontSize: '24px' }}>
               정말 회원 탈퇴를 하시겠습니까?
             </h2>
-            <div className="flex justify-center mt-16 space-x-60">
+            <div className="flex justify-center mt-16 space-x-28">
               <button
                 onClick={handleWithdrawal}
-                className="w-32 px-4 py-3 text-white bg-blue-600 rounded"
+                className="w-32 px-4 py-3 text-white rounded bg-solive-200"
               >
                 네
               </button>
