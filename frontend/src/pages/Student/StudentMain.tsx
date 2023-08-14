1194a4761e6436e8a3fe10e65e6992b48e9bb361
박민혁 <sis03090@naver.com>
2023-08-14 12:16:33 +0900

    Fix: API 수정에 의한 학생 메인페이지 수정
    - 문제 관리 api 수정으로 인한 수정

--- frontend/src/pages/Student/StudentMain.tsx
+++ frontend/src/pages/Student/StudentMain.tsx
@@ -80,14 +80,34 @@ const Student = () => {
   useEffect(() => {
     const fetchProblems = async () => {
       try {
-        const result = await getMyProblems(user.accessToken);
+        const result = await getMyProblems(
+          user.userId,
+          1,
+          1100,
+          0,
+          1,
+          '',
+          'TIME_ASC',
+          0,
+          user.accessToken,
+        );
         if (result.success) {
           setProblems(result.data);
         } else if (result.error === 'JWT_TOKEN_EXPIRED_EXCEPTION') {
           const newAccessToken = await getNewAccessToken(user.refreshToken);
           if (newAccessToken) {
             setUser({ ...user, accessToken: newAccessToken });
-            const newResult = await getMyProblems(newAccessToken);
+            const newResult = await getMyProblems(
+              user.userId,
+              1,
+              1100,
+              0,
+              1,
+              '',
+              'TIME_ASC',
+              0,
+              newAccessToken,
+            );
             if (newResult.success) {
               setProblems(newResult.data);
             } else {
@@ -120,7 +140,7 @@ const Student = () => {
           const newAccessToken = await getNewAccessToken(user.refreshToken);
           if (newAccessToken) {
             setUser({ ...user, accessToken: newAccessToken });
-            const newResult = await getMyProblems(newAccessToken);
+            const newResult = await getTeachers(newAccessToken);
             if (newResult.success) {
               setTeachers(newResult.data);
             } else {
@@ -146,7 +166,7 @@ const Student = () => {
   return (
     <div className="flex flex-col items-center">
       {isLoading}
-      <div className="w-full overflow-hidden h-48 flex items-center justify-center bg-gray-200 text-2xl font-bold mb-8 relative">
+      <div className="relative flex items-center justify-center w-full h-48 mb-8 overflow-hidden text-2xl font-bold bg-gray-200">
         {banners.map((banner, idx) => (
           <div
             key={idx}
@@ -161,9 +181,9 @@ const Student = () => {
         ))}
       </div>
       <div className="flex flex-row w-full">
-        <div className="w-1/2 p-4 text-center border-r-2 border-gray-200 flex flex-col justify-between">
+        <div className="flex flex-col justify-between w-1/2 p-4 text-center border-r-2 border-gray-200">
           <div>
-            <h2 className="text-xl font-bold mb-4 mt-4">
+            <h2 className="mt-4 mb-4 text-xl font-bold">
               현재 접속중인 선생님 목록
             </h2>
             <div className="grid grid-cols-3 gap-4">
@@ -185,8 +205,8 @@ const Student = () => {
             </div>
           </div>
         </div>
-        <div className="w-1/2 p-4 text-center border-r-2 border-gray-200 flex flex-col justify-between">
-          <h2 className="text-xl font-bold mb-4 mt-4">내가 등록한 문제</h2>
+        <div className="flex flex-col justify-between w-1/2 p-4 text-center border-r-2 border-gray-200">
+          <h2 className="mt-4 mb-4 text-xl font-bold">내가 등록한 문제</h2>
           <div className="flex flex-col items-center justify-center h-full mb-16">
             <div className="grid grid-cols-3 gap-4">
               {problems.slice(0, 3).map((problem) => (
@@ -204,15 +224,15 @@ const Student = () => {
                 </div>
               ))}
             </div>
-            <div className="mt-20 flex justify-center space-x-4 ">
+            <div className="flex justify-center mt-20 space-x-4 ">
               <button
-                className="bg-blue-500 text-white px-4 py-2 rounded"
+                className="px-4 py-2 text-white bg-blue-500 rounded"
                 onClick={() => navigate('questionregistration')}
               >
                 문제 등록하기
               </button>
               <button
-                className="bg-blue-500 text-white px-4 py-2 rounded"
+                className="px-4 py-2 text-white bg-blue-500 rounded"
                 onClick={() => navigate('/mypage/questionmanagement')}
               >
                 전체 보기
